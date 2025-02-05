3e0d548f12496aa6e7b0ce12d9f0151a7bd04cd3
junah201 <junah.dev@gmail.com>
2025-02-06 03:15:08 +0900

    🔧 Update format script to include docs_src

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
 
-ruff check nexify scripts tests --fix
-ruff format nexify scripts tests
+ruff check nexify scripts tests docs_src --fix
+ruff format nexify scripts tests docs_src
